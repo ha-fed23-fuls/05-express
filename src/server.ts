3a465cd5bcd308697768deef8fe383b264487e2c
David Andersson <lejonmanen@gmail.com>
2024-09-02 11:38:18 +0200

    POST request

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express'
 const app: Express = express()
 const port = 1337
 
+let counter = 0
 
 // Endpoints
 app.get('/', (req: Request, res: Response) => {
@@ -9,6 +10,19 @@ app.get('/', (req: Request, res: Response) => {
 })
 app.get('/today', (req: Request, res: Response) => {
 	res.send('Today is a Monday.')
+})
+app.get('/counter', (req: Request, res: Response) => {
+	counter++
+	res.send( String(counter) )
+})
+
+let cars: string[] = []
+app.get('/cars', (req: Request, res: Response) => {
+	res.send(cars)
+})
+app.post('/cars', (req: Request, res: Response) => {
+	cars.push('volvo')
+	res.sendStatus(200)
 })
 
 
