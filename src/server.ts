84e06ca74feefbe28d7be3f2b09150a76a7c3574
David Andersson <lejonmanen@gmail.com>
2024-09-02 11:14:10 +0200

    GET endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,19 @@
+import express, { Express, Request, Response } from 'express'
+const app: Express = express()
+const port = 1337
 
-console.log('Hello from server')
+
+// Endpoints
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello! Glad you could join us.')
+})
+app.get('/today', (req: Request, res: Response) => {
+	res.send('Today is a Monday.')
+})
+
+
+app.listen(port, () => {
+	console.log('Server is listening on port ' + port)
+})
+
+//  localhost://5173
